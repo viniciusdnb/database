b0ed637cf7fd2f1dd7c1eba406371008228136bb
vini <viniciusdnb@hotmail.com>
2022-08-06 22:53:40 -0300

    criando o metodo que retorna json nas consultas

--- src/Models/AbstractModel.php
+++ src/Models/AbstractModel.php
@@ -10,6 +10,7 @@
 	abstract class AbstractModel
 	{
 		
+		//funcao que retorna objeto padrao do php
 		function select(string $where = null)
 		{
 			$model = new SelectModel($this->table, $this->columns);
@@ -18,6 +19,38 @@
 
 			return $model->getResult();
 		}
+
+		//funcao que retorna um json como resultado da consulta
+		function selectJson(string $where = null)
+		{
+			$table = $this->select($where);
+
+			$json = [];
+
+			foreach($table as $value)
+			{
+				$json[$value->{$this->columns[0]}] = $this->arr($value, $this->columns);
+			}
+
+			return json_encode($json, JSON_FORCE_OBJECT);
+		}
+
+		//funcao que retorna um array somente com o resultado da pesquisa
+		//onde cada indce do array é o nume da coluna
+		private function arr($value, $colums)
+		{
+			$arr = [];
+
+			for($i = 0; $i < count($colums); $i++)
+			{
+				//{}serve para chamar o atributo da classe atraves da string passada
+				$arr[$colums[$i]] = $value->{$colums[$i]};
+			}
+
+			return $arr;
+		}
+
+
 
 		function update(array $values, string $where, bool $arr_shift = false)
 		{
